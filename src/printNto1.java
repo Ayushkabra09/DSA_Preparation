58b125e8bb47ac0fdf449444bbda9611e41dc14c
Ayush Kabra <ayush.kabra09@gmail.com>
2023-06-05 07:45:44 +0530

    DSA programs

---
+++ src/printNto1.java
@@ -0,0 +1,15 @@
+public class printNto1 {
+
+    public static void main(String[] args){
+        fun(6);
+    }
+
+    static void fun(int n){
+        if(n==0)
+            return;
+
+        System.out.println(n);
+        fun(n-1);
+    }
+
+}
